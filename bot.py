b86e08db94a326ecf6c4aeae0718b27ea8a0b57b
unkownfire <unkownfire@hotmail.com>
2020-01-22 14:45:27 -0500

    Added full database functionaility, verified that all database functions work.
    Adding and removing users from secret santa works, assigning users their 
    secret santa with "!santa Begin" is currently not functional.

--- bot.py
+++ bot.py
@@ -3,13 +3,14 @@ import discord
 import time
 import asyncio
 import functions
-import databaseControl
+from databaseControl import *
 #TOKEN = 'NjMwODE5NTk1Mzc2MTMyMTM5.XdDVAQ.hkTME3OmvmqFnlNqcg0HCKZnQSU'
-TOKEN = 'NjQ3NTY1Njk1NDI0NjU5NDg3.XgKlsg.hk78CHf_6zL1iLV1p8tzoU4Ll7I'
+TOKEN = 'NjQ3NTY1Njk1NDI0NjU5NDg3.XiKfkQ.myHUG3GF0yCd7NWVqJcei7GlJzU'
 #TOKEN = 'NjMwODE5NTk1Mzc2MTMyMTM5.XZuLOw.JBPbCurmDyquUzqNu7Q7FhRTcmw'
 client = discord.Client()
-
 messages = joined = 0
+
+
 async def update_stats():
     await client.wait_until_ready()
     global messages, joined
@@ -37,21 +38,32 @@ async def on_message(message):
         #if message.author == client.user:
         #    return
         if message.content.startswith('!santa Join'):
-            if(str(message.author) in joinedSanta):
+            if(SelectUser(message.author.id,message.guild.id)[0][3]):
                 msg = f"{message.author.mention} is already in the Secret Santa list"
                 await message.channel.send(msg)
             else:
-                joinedSanta.append(str(message.author))
+                UpdateUser(User(message.author.id, message.author.name, message.guild.id, True))
                 msg = f"Added {message.author.mention} to Secret Santa list"
                 await message.channel.send(msg)
         if message.content.startswith('!santa Leave'):
-            if(str(message.author) in joinedSanta):
+            if(SelectUser(message.author.id,message.guild.id)[0][3]):
                 msg = f"Removed {message.author.mention} from Secret Santa list"
-                joinedSanta.remove(str(message.author))
+                UpdateUser(User(message.author.id, message.author.name, message.guild.id, False))
                 await message.channel.send(msg)
             else:
                 msg = f"{message.author.mention} is not in the Secret Santa list"
                 await message.channel.send(msg)
+        if message.content.startswith('!santa Begin'):
+            santaList = SelectSantaUsers()
+            incrementalList = [x for x in range(len(santaList))]
+            santaIndices = santa(incrementalList)
+            for pair in santaIndices:
+                await get_user(santaList[pair[0]][0]).send(f"You are {santaList[pair[1]][1]}s secret santa!")
+        if message.content.startswith('!santa Users'):
+            santaList = SelectSantaUsers()
+            for user in santaList:
+                await message.channel.send(user[1])
+
         if message.content.startswith('!hello'):
             msg = f"Hello {message.author.mention}"
             await message.channel.send(msg)
@@ -62,7 +74,9 @@ async def on_message(message):
 
 
 #@client.event
-#async def on_member_join(member):
+async def on_member_join(member):
+    InsertUser(User(member.id,member.name,member.guild.id,False))
+
 #    global joined
 #    joined += 1
 #    welcome = client.get_channel(570466644611039244)
@@ -71,7 +85,40 @@ async def on_message(message):
 
 @client.event
 async def on_ready():
-        print("Logged in as", client.user.name, client.user.id)
+    print("Logged in as", client.user.name, client.user.id)
+    #Upon startup, go through all guilds and update all tables accordingly
+    guilds = client.guilds
+    print(guilds)
+    for g in guilds:
+        #check is used to see if the guild already exists in our GUILD table
+        check = SelectGuild(g.id)
+        if(len(check)==0):
+            newGuild = Guild(g.id,g.name)
+            InsertGuild(newGuild)
+        elif(check[0][1] != g.name):
+            newGuild = Guild(g.id,g.name)
+            UpdateGuild(newGuild)
+        for u in g.members:
+            #userCheck is used to see if our user already exists in our USER table
+            userCheck = SelectUser(u.id,g.id)
+            print(userCheck)
+            if(len(userCheck)==0):
+                newUser = User(u.id,u.name,g.id,False)
+                InsertUser(newUser)
+            elif(userCheck[0][1] != u.name):
+                #if the user has new data, we update it
+                UpdateUser(User(u.id,u.name,g.id, userCheck[0][3]))
+        for c in g.channels:
 
-client.loop.create_task(update_stats())
+            channelCheck = SelectChannel(c.id, g.id)
+            print(channelCheck)
+            if(len(channelCheck) == 0):
+                newChannel = Channel(c.id,c.name,True,g.id)
+                InsertChannel(newChannel)
+                #if the channel has new data, update it
+            elif(channelCheck[0][1] != c.name):
+                newChannel = Channel(c.id,c.name,channelCheck[0][3],g.id)
+                UpdateChannel(newChannel)
+
+#client.loop.create_task(update_stats())
 client.run(TOKEN)
--- databaseControl.py
+++ databaseControl.py
@@ -1,5 +1,7 @@
 import sqlite3
+import discord
 from sqlite3 import Error
+database = r"discord.db"
 def create_connection(db):
     conn = None
     try:
@@ -9,35 +11,45 @@ def create_connection(db):
         print(e)
 
     return conn
+
 class User:
-    def __init__(id, name, guild_id, santa):
+    def __init__(self, id, name, guild_id, santa):
         self.userId = id
         self.userName = name
         self.userGuildId = guild_id
         self.santa = santa
 class Guild:
-    def __init__(id, name):
-        self.guildId = guild_id
+    def __init__(self, id, name):
+        self.guildId = id
         self.guildName = name
 class Channel:
-    def __init__(id, name, guild_id):
+    def __init__(self, id, name, readFrom,guild_id):
         self.channelName = name
+        self.channelReadable = readFrom
         self.channelId = id
         self.channelGuildId =guild_id
-
-def executeQuery(db, query):
-    conn = create_connection(db)
+#Computes non SELECT statements like INSERT, DELETE, UPDATE
+def executeQuery(query):
+    conn = create_connection(database)
     if conn is not None:
         try:
             c = conn.cursor()
             c.execute(query)
         except Error as e:
             print(e)
+        conn.commit()
         conn.close()
     else:
         print("Error! cannot create the database connection.")
-
-
+#Returns the results of SELECT statements
+def executeSelect(query):
+    conn = create_connection(database)
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
 def create_table(conn, create_table_sql):
     """ create a table from the create_table_sql statement
     :param conn: Connection object
@@ -50,17 +62,18 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 def dbCreateTables():
-    database = r"discord.db"
+    #database = r"discord.db"
     sqlite3CreateGuildsTable = """
         CREATE TABLE IF NOT EXISTS guild (
-            id integer PRIMARY KEY,
+            id integer PRIMARY KEY DESC,
             name text NOT NULL
         );
         """
     sqlite3CreateChannelsTable = """
         CREATE TABLE IF NOT EXISTS channel (
-            id integer PRIMARY KEY,
+            id integer PRIMARY KEY DESC,
             name text NOT NULL,
+            readFrom BOOLEAN,
             guild_id INTEGER NOT NULL,
             FOREIGN KEY (guild_id) REFERENCES guild (id)
         )
@@ -72,7 +85,7 @@ def dbCreateTables():
   			guild_id INTEGER NOT NULL,
             santa BOOLEAN NOT NULL,
             FOREIGN KEY (guild_id) REFERENCES guild (id),
-            PRIMARY KEY (id, name, guild_id)
+            PRIMARY KEY (id, guild_id)
 
         )
     """
@@ -93,59 +106,133 @@ def dbCreateTables():
     else:
         print("Error! cannot create the database connection.")
 def UpdateUser(newUserData):
-    database = r"discord.db"
+    #database = r"discord.db"
     sqlite3UpdateUser = f"""
         UPDATE USER
-        SET name = {newUserData.userName},
-        santa = {newUserData.santa},
+        SET name = \"{newUserData.userName}\",
+        santa = {newUserData.santa}
         WHERE id = {newUserData.userId}
         AND guild_id = {newUserData.userGuildId}
     """
     executeQuery(sqlite3UpdateUser)
 def UpdateGuild(newGuildData):
-    database = r"discord.db"
+    #database = r"discord.db"
     sqlite3UpdateGuild = f"""
         UPDATE GUILD
-        SET name = {newGuildData.guildName}
+        SET name = \"{newGuildData.guildName}\"
         WHERE id = {newGuildData.guildId}
     """
-    executeQuery(database, sqlite3UpdateGuild)
+    executeQuery(sqlite3UpdateGuild)
 def UpdateChannel(newChannelData):
-    database = r"discord.db"
+    #database = r"discord.db"
     sqlite3UpdateChannel = f"""
         UPDATE CHANNEL
-        SET name = {newChannelData.channelName}
+        SET name = \"{newChannelData.channelName}\",
+        readFrom = {newChannelData.channelReadable}
         WHERE id = {newChannelData.channelId}
         AND guild_id = {newChannelData.channelGuildId}
     """
-    executeQuery(database, sqlite3UpdateChannel)
+    executeQuery(sqlite3UpdateChannel)
 def InsertUser(newUserData):
-    database = r"discord.db"
+    #database = r"discord.db"
     sqlite3InsertUser = f"""
         INSERT INTO USER (id, name, santa, guild_id)
         VALUES
-            {newUserData.userId},
-            {newUserData.userName},
+            ({newUserData.userId},
+            \"{newUserData.userName}\",
             {newUserData.santa},
-            {newUserData.guild_id}
+            {newUserData.userGuildId})
     """
     executeQuery(sqlite3InsertUser)
 def InsertGuild(newGuildData):
-    database = r"discord.db"
+    #database = r"discord.db"
     sqlite3InsertGuild = f"""
         INSERT INTO GUILD (id, name)
         VALUES
-        {newGuildData.guildId},
-        {newGuildData.guildName}
+        ({newGuildData.guildId},
+        \"{newGuildData.guildName}\");
     """
+
     executeQuery(sqlite3InsertGuild)
 def InsertChannel(newChannelData):
-    database = r"discord.db"
+    #database = r"discord.db"
     sqlite3InsertChannel = f"""
-        INSERT INTO CHANNEL (id, name, guild_id)
+        INSERT INTO CHANNEL (id, name,readFrom, guild_id)
         VALUES
-            {newChannelData.channelId},
-            {newChannelData.channelName},
-            {newChannelData.channelGuildId}
+            ({newChannelData.channelId},
+            \"{newChannelData.channelName}\",
+            {newChannelData.channelReadable},
+            {newChannelData.channelGuildId})
     """
     executeQuery(sqlite3InsertChannel)
+#returns all user tuples of a given guild
+def SelectAllUsers(guildId):
+
+    sqlite3SelectUsers = f"""
+        SELECT *
+        FROM User
+        WHERE guild_id = {guildId}
+    """
+    return executeSelect(sqlite3SelectUsers)
+#returns all guild tuples
+def SelectAllGuilds():
+    #database = r"discord.db"
+    sqlite3SelectGuilds = f"""
+        SELECT *
+        FROM GUILD
+    """
+    return executeSelect(sqlite3SelectGuilds)
+#returns all channel tuples of a given guild
+def SelectAllChannels(guildId):
+    sqlite3SelectChannels = f"""
+        SELECT *
+        FROM CHANNEL
+        WHERE guild_id = {guildId}
+    """
+    return executeSelect(sqlite3SelectChannels)
+def SelectSanta(guildId):
+    sqlite3SelectUsers = f"""
+        SELECT *
+        FROM User
+        WHERE guild_id = {guildId}
+        AND santa = true
+    """
+    return executeSelect(sqlite3SelectUsers)
+#Query to select single guild based off primary key
+def SelectGuild(guildId):
+    sqlite3SelectGuild = f"""
+        SELECT *
+        FROM Guild
+        WHERE id = {guildId}
+        """
+    #Returns single guild tuple
+    return executeSelect(sqlite3SelectGuild)
+#Query to select single User based off primary keys
+def SelectUser(userId, guildId):
+
+    sqlite3SelectUser = f"""
+        SELECT *
+        FROM User
+        WHERE guild_id = {guildId}
+        AND id = {userId}
+    """
+    #Returns single user tuple
+    return executeSelect(sqlite3SelectUser)
+#query to select a single user who is in a secret santa list
+def SelectSantaUsers():
+    sqlite3SelectSantaUser = f"""
+        SELECT *
+        FROM User
+        WHERE santa = True
+        """
+    return executeSelect(sqlite3SelectSantaUser)
+#query to select single channel based off primary keys
+def SelectChannel(channelId, guildId):
+    sqlite3SelectChannel = f"""
+        SELECT *
+        FROM CHANNEL
+        WHERE id = {channelId}
+        AND guild_id = {guildId}
+    """
+    #Returns single channel tuple from above query
+    return executeSelect(sqlite3SelectChannel)
